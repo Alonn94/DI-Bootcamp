449d3f59c44eeea6ed3a038c201c400be9ac9391
Alonn Barthels <alonnbarthels@Alonns-MacBook-Air-2.local>
2025-02-25 07:15:58 +0200

    second push

--- Week4/Day2/Exercisesxp/Dailychallenge.js
+++ Week4/Day2/Exercisesxp/Dailychallenge.js
@@ -26,7 +26,7 @@ for (let i = 0; i <= 6; i++) {
     for (let j = 0; j <= i; j++) {
         pattern += "* ";
     }
-    pattern += "\n"; // Move to the next line
+    pattern += "\n";
 }
 
 console.log(pattern);
